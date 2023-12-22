001fca807e5d4a2232a965c3611207de08ff975f
Link Dupont <link@sub-pop.net>
2023-12-22 12:23:59 -0500

    Update Queue to use generic type constraint

--- queue.go
+++ queue.go
@@ -1,28 +1,28 @@
 package adt
 
 // Element is an element of a queue.
-type Element struct {
-	next, prev *Element
-	Value      interface{}
+type Element[V any] struct {
+	next, prev *Element[V]
+	Value      V
 }
 
 // Queue represents an ordered collection of elements that can be accessed in a
 // first-in, first-out manner. The zero value for Queue is an empty queue ready
 // to use.
-type Queue struct {
-	front, back *Element
+type Queue[V any] struct {
+	front, back *Element[V]
 	len         int
 }
 
 // NewQueue returns an initialized queue.
-func NewQueue() *Queue {
-	return &Queue{}
+func NewQueue[V any]() *Queue[V] {
+	return &Queue[V]{}
 }
 
 // Enqueue inserts a new element e with value v to the back of queue q and
 // returns e.
-func (q *Queue) Enqueue(v interface{}) *Element {
-	e := &Element{
+func (q *Queue[V]) Enqueue(v V) *Element[V] {
+	e := &Element[V]{
 		Value: v,
 		next:  q.back,
 	}
@@ -37,17 +37,17 @@ func (q *Queue) Enqueue(v interface{}) *Element {
 }
 
 // Dequeue removes and returns the front-most element of queue q.
-func (q *Queue) Dequeue() interface{} {
+func (q *Queue[V]) Dequeue() *V {
 	e := q.front
 	if q.len == 0 {
 		return nil
 	}
 	q.front = q.front.prev
 	q.len--
-	return e.Value
+	return &e.Value
 }
 
 // Len returns the number of elements in the queue.
-func (q *Queue) Len() int {
+func (q *Queue[V]) Len() int {
 	return q.len
 }
--- queue_test.go
+++ queue_test.go
@@ -4,25 +4,50 @@ import (
 	"testing"
 )
 
-func TestQueue(t *testing.T) {
+func TestQueueString(t *testing.T) {
 	tests := []struct {
-		input, want []interface{}
+		input, want []string
 	}{
 		{
-			input: []interface{}{"a", "b", "c"},
-			want:  []interface{}{"a", "b", "c"},
+			input: []string{"a", "b", "c"},
+			want:  []string{"a", "b", "c"},
 		},
 	}
 
 	for _, test := range tests {
-		var q Queue
+		var q Queue[string]
 		for _, i := range test.input {
 			q.Enqueue(i)
 		}
 
 		for _, want := range test.want {
 			got := q.Dequeue()
-			if got != want {
+			if *got != want {
+				t.Errorf("%+v != %+v", got, want)
+			}
+		}
+	}
+}
+
+func TestQueueInt(t *testing.T) {
+	tests := []struct {
+		input, want []int
+	}{
+		{
+			input: []int{1, 2, 3},
+			want:  []int{1, 2, 3},
+		},
+	}
+
+	for _, test := range tests {
+		var q Queue[int]
+		for _, i := range test.input {
+			q.Enqueue(i)
+		}
+
+		for _, want := range test.want {
+			got := q.Dequeue()
+			if *got != want {
 				t.Errorf("%+v != %+v", got, want)
 			}
 		}
